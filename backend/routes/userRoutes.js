8a5b5e55d7d1fb1b8c536947982754efdf34b87b
khaled2wz <khaledalhzmy2@gmail.com>
2025-02-15 16:16:38 +0300

    Reapply "fixed user pic issue, configuring the code"
    This reverts commit b97e3f73a06e8e1ab15ce3021eb1d215b752ccd9.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { registerUser, loginUser, sendVerificationCode, verifyCodeAndResetPassword, resetPassword, updateUserProfile, upload } = require('../controllers/userController');
 const { protect } = require('../middleware/authMiddleware');
+const { deleteProfilePic } = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/register', registerUser);
@@ -8,6 +9,7 @@ router.post('/login', loginUser);
 router.post('/send-verification-code', sendVerificationCode);
 router.post('/verify-code-and-reset-password', verifyCodeAndResetPassword);
 router.post('/reset-password', resetPassword);
+router.delete('/me/profile-pic',deleteProfilePic);
 router.get('/me', protect, async (req, res) => {
   res.json(req.user);
 });
